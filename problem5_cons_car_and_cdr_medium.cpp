ba4537efa765f4ccdb227f1f9b3d8a458ce8a6bd
Chase Busacker <cwbusacker@hotmail.com>
2020-10-03 11:45:44 -0600

    Problem 5 completed.

---
+++ problem5_cons_car_and_cdr_medium.cpp
@@ -0,0 +1,45 @@
+/*This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+ For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr. */
+#include <map>
+#include <iostream>
+using std::pair;
+using std::cin;
+using std::cout;
+std::pair <int, int> cons(int a, int b)
+{
+    return std::pair<int, int> (a,b);
+}
+
+int car(std::pair <int, int> p)
+{
+    return p.first;
+}
+
+int cdr(std::pair <int, int> p)
+{
+    return p.second;
+}
+
+int main()
+{
+    int x, y;
+    cout << "Please enter the left side of your pair: ";
+    cin >> x;
+    cout << "Please enter the right side of your pair: ";
+    cin >> y;
+
+    cout << "\n";
+
+    cout << "The left side is: " << car(cons(x, y)) << "\n";
+    cout << "The right side is : " << cdr(cons(x, y)) << "\n";
+}
